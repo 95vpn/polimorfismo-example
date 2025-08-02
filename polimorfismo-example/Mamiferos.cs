6755ed955240c6ba14b43a6fb552b91539c71fc9
Johnny Herrera <jmjaramillo95@gmail.com>
2025-08-01 22:44:33 -0500

    Agregar archivos de proyecto.

---
+++ polimorfismo-example/Mamiferos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polimorfismo_example
+{
+    public class Mamiferos
+    {
+        public Mamiferos(String nombre)
+        {
+            nombreSerVivo = nombre;
+        }
+
+        public void respirar()
+        {
+            Console.WriteLine("Soy capaz de respirar");
+        }
+
+        public void cuidarCrias()
+        {
+            Console.WriteLine("Cuido de mis crias hasta que se valgan ");
+        }
+
+    }
+}

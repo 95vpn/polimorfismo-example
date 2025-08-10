78e50c3328d641bdae4655c0f9ad395fa7c8639b
Johnny Herrera <jmjaramillo95@gmail.com>
2025-08-09 22:31:47 -0500

    interfaces

---
+++ polimorfismo-example/Ballena.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polimorfismo_example
+{
+    public class Ballena : Mamiferos
+    {
+        public Ballena(String nombreBallena) : base(nombreBallena)
+        { 
+
+        }
+
+        public void Nadar()
+        {
+            Console.WriteLine("Soy capaz de nadar");
+        }
+    }
+}
